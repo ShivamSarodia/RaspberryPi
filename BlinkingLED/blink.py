511ae984eef57a86a4de629e3723648f05ad822e
Shivam Sarodia <ssarodia@gmail.com>
2015-11-23 06:01:42 +0000

    Generalized code slightly

--- BlinkingLED/blink.py
+++ BlinkingLED/blink.py
@@ -1,11 +1,13 @@
 import RPi.GPIO as GPIO
 import time
 
+pins = [7]
+
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.OUT)
+GPIO.setup(pins[0], GPIO.OUT)
 
 while True:
     time.sleep(1)
-    GPIO.output(12, 1)
+    GPIO.output(pins[0], 1)
     time.sleep(1)
-    GPIO.output(12, 0)
+    GPIO.output(pins[0], 0)
