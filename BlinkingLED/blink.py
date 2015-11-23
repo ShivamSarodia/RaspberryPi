b7d37a9702939af6a21e5731e35c1a5b7ecc239b
Shivam Sarodia <ssarodia@gmail.com>
2015-11-23 00:45:31 -0500

    Fixed silly error

--- BlinkingLED/blink.py
+++ BlinkingLED/blink.py
@@ -2,8 +2,10 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.OUT)
 
 while True:
-    GPIO.setup(12, GPIO.OUT)
+    time.sleep(1)
     GPIO.output(12, 1)
     time.sleep(1)
+    GPIO.output(12, 0)
